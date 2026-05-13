085aca494b84341050b66707c863bf5e6a2953e6
zhikanyeye <ai2845612055@outlook.com>
2026-05-14 02:29:40 +0800

    Fix GitHub Pages module paths

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig, loadEnv } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -13,8 +12,8 @@ export default defineConfig(({ mode }) => {
       emptyOutDir: true,
       rollupOptions: {
         input: {
-          main: resolve(__dirname, 'index.html'),
-          player: resolve(__dirname, 'player.html'),
+          main: 'index.html',
+          player: 'player.html',
         },
       },
     },
